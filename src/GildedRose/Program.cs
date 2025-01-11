4944a2face35fddc4511264d7944689701e7b390
Warren Lockhart <wlockhart55@gmail.com>
2025-01-11 14:51:44 +0000

    Replaced GildedRose for loop with foreach and extracted UpdateItem method

--- src/GildedRose/Program.cs
+++ src/GildedRose/Program.cs
@@ -9,7 +9,8 @@ namespace GildedRoseKata
         {
             Console.WriteLine("Welcome to Gilded Rose!");
 
-            IList<Item> Items = new List<Item>{
+            var Items = new List<Item>
+            {
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
@@ -43,11 +44,14 @@ namespace GildedRoseKata
             {
                 Console.WriteLine("-------- day " + i + " --------");
                 Console.WriteLine("name, sellIn, quality");
+
                 for (var j = 0; j < Items.Count; j++)
                 {
-                    System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+                    Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
                 }
+
                 Console.WriteLine("");
+
                 app.UpdateQuality();
             }
         }
